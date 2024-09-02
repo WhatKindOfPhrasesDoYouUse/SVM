6f0e7fe05a78890d9bc8cb1d49fe93aefec7346a
Dima Oshuev <mazafon64@gmail.com>
2024-09-03 01:11:44 +0300

    Training with a given number C

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def first_dataset():
 def first_data_display():
     svm.visualize_boundary_linear(first_dataset()[0], first_dataset()[1], None, 'Исходные данные dataset1')
 
-def learn_dataset1():
-    model = svm.svm_train(first_dataset()[0], first_dataset()[1], 1, svm.linear_kernel, 0.001, 20)
-    svm.visualize_boundary_linear(first_dataset()[0], first_dataset()[1], model, 'Разделяющая граница при C=1')
+def learn_dataset1(C: int):
+    model = svm.svm_train(first_dataset()[0], first_dataset()[1], C, svm.linear_kernel, 0.001, 20)
+    svm.visualize_boundary_linear(first_dataset()[0], first_dataset()[1], model, 'Разделяющая граница при C={}'.format(C))
 
 def main():
     first_data_display()
-    learn_dataset1()
+    learn_dataset1(1)
+    learn_dataset1(100)
 
 if __name__ == '__main__':
     main()
