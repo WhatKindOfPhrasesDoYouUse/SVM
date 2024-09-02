2e0cbf24d4839a1bf6f6db125b1f2eb43f621647
Dima Oshuev <mazafon64@gmail.com>
2024-09-03 01:34:18 +0300

    dataset3 are displayed on the graph

--- main.py
+++ main.py
@@ -14,11 +14,23 @@ def second_dataset():
     x = data['X']
     return x, y
 
+def third_dataset():
+    data = sio.loadmat('dataset3.mat')
+    y = data['y'].astype(np.float64)
+    x = data['X']
+    x_val = data['Xval']
+    y_val = data['yval'].astype(np.float64)
+    return x, y, x_val, y_val
+
 def first_data_display():
     svm.visualize_boundary_linear(first_dataset()[0], first_dataset()[1], None, 'Исходные данные dataset1')
 
 def second_data_display():
     svm.visualize_boundary_linear(second_dataset()[0], second_dataset()[1], None, 'Исходные данные dataset2')
+
+def third_data_display():
+    svm.visualize_boundary_linear(third_dataset()[0], third_dataset()[1], None, 'Исходные данные обучающей выборки')
+    svm.visualize_boundary_linear(third_dataset()[2], third_dataset()[3], None, 'Исходные данные тестовой выборки')
 
 def learn_dataset1(C: int):
     model = svm.svm_train(first_dataset()[0], first_dataset()[1], C, svm.linear_kernel, 0.001, 20)
@@ -40,7 +52,8 @@ def main():
     #gaussian_kernel_display(1)
     #gaussian_kernel_display(3)
     #second_data_display()
-    learn_dataset2(1.0, 0.1)
+    #learn_dataset2(1.0, 0.1)
+    third_data_display()
 
 if __name__ == '__main__':
     main()
