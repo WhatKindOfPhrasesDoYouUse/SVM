c1fa4b57796b2e42f8f2f5f9fb9ac6476bb8c690
Dima Oshuev <mazafon64@gmail.com>
2024-09-03 01:22:40 +0300

    dataset2 are displayed on the graph

--- main.py
+++ main.py
@@ -8,23 +8,33 @@ def first_dataset():
     x = data['X']
     return x, y
 
+def second_dataset():
+    data = sio.loadmat('dataset2.mat')
+    y = data['y'].astype(np.float64)
+    x = data['X']
+    return x, y
+
 def first_data_display():
     svm.visualize_boundary_linear(first_dataset()[0], first_dataset()[1], None, 'Исходные данные dataset1')
+
+def second_data_display():
+    svm.visualize_boundary_linear(second_dataset()[0], second_dataset()[1], None, 'Исходные данные dataset2')
 
 def learn_dataset1(C: int):
     model = svm.svm_train(first_dataset()[0], first_dataset()[1], C, svm.linear_kernel, 0.001, 20)
     svm.visualize_boundary_linear(first_dataset()[0], first_dataset()[1], model, 'Разделяющая граница при C={}'.format(C))
 
-def gaussian_kernel_display(n: int):
-    svm.contour(n)
+def gaussian_kernel_display(sigma: int):
+    svm.contour(sigma)
 
 
 def main():
-    first_data_display()
-    learn_dataset1(1)
-    learn_dataset1(100)
-    gaussian_kernel_display(1)
-    gaussian_kernel_display(3)
+    #first_data_display()
+    #learn_dataset1(1)
+    #learn_dataset1(100)
+    #gaussian_kernel_display(1)
+    #gaussian_kernel_display(3)
+    second_data_display()
 
 if __name__ == '__main__':
     main()
